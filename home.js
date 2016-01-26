67e8a7378617959515989c9c7344e5bad74f4f80
ctevnan <carolyntevnan@yahoo.com>
2016-01-25 20:35:12 -0500

    added home.html and home.js

---
+++ home.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var myName = prompt("What is your name?");
+  var myColor = prompt("What is your favorite color?");
+ // var mySentence = myName + "likes" + myColor;
+ // alert(mySentence);
+}
